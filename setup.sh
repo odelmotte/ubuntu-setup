b7b27f4d463e3e45329b44172d26a7bff1aa0fef
hangar18rip <hangar18rip@users.noreply.github.com>
2024-01-23 22:16:39 +0100

    init

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+###################################
+# Install git
+
+sudo apt update && sudo apt install git -y
+
+git config --global user.email "hangar18rip@users.noreply.github.com"
+git config --global user.name "hangar18rip"
+
+###################################
+# Install gh cli
+
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh -y
+
+###################################
+# Install PowerShell Core & .Net
+
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+source /etc/os-release
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+
+sudo apt-get install -y powershell dotnet-sdk-8.0
+
+###################################
+# Install terraform
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
+###################################
+# Install Azure CLI
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
